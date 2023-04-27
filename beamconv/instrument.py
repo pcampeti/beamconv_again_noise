af94e20556535fc7ed6b02b857147b67ea731e34
Paolo Campeti <36236108+pcampeti@users.noreply.github.com>
2023-04-27 15:09:02 +0200

    added noise parameters in scan_instrument_mpi and noise parameters and noise tod in bin_tod

--- beamconv/instrument.py
+++ beamconv/instrument.py
@@ -1726,7 +1726,9 @@ class ScanStrategy(Instrument, qp.QMap):
             create_memmap=False, scatter=True, reuse_spinmaps=False,
             interp=False, save_tod=False, save_point=False, ctalk=0.,
             preview_pointing=False, filter_4fhwp=False, input_v=False,
-            beam_v=False, filter_highpass=False, **kwargs):
+            beam_v=False, filter_highpass=False, noise_tod=False, sigma=None,
+            f_min=None, f_knee=None, f_samp=None, slope=None, 
+            **kwargs):
         '''
         Loop over beam pairs, calculate boresight pointing
         in parallel, rotate or modulate instrument if
@@ -1963,6 +1965,12 @@ class ScanStrategy(Instrument, qp.QMap):
                             self.bin_tod(beam_a, add_to_global=True, 
                                          filter_4fhwp=filter_4fhwp, 
                                          filter_highpass=filter_highpass,
+                                         noise_tod=noise_tod,
+                                         sigma=sigma, 
+                                         f_min=f_min, 
+                                         f_knee=f_knee, 
+                                         f_samp=f_samp, 
+                                         slope=slope,
                                          **subchunk)
 
                     if beam_b and not beam_b.dead:
@@ -1978,6 +1986,12 @@ class ScanStrategy(Instrument, qp.QMap):
                             self.bin_tod(beam_b, add_to_global=True, 
                                          filter_4fhwp=filter_4fhwp, 
                                          filter_highpass=filter_highpass,
+                                         noise_tod=noise_tod,
+                                         sigma=sigma, 
+                                         f_min=f_min, 
+                                         f_knee=f_knee, 
+                                         f_samp=f_samp, 
+                                         slope=slope,
                                          **subchunk)
 
                     if do_ctalk:
@@ -1991,10 +2005,24 @@ class ScanStrategy(Instrument, qp.QMap):
                         if binning:
                             self.bin_tod(beam_a, tod=tod_a, add_to_global=True,
                             filter_4fhwp=filter_4fhwp, 
-                            filter_highpass=filter_highpass, **subchunk)
+                            filter_highpass=filter_highpass,
+                            noise_tod=noise_tod,
+                            sigma=sigma, 
+                            f_min=f_min, 
+                            f_knee=f_knee, 
+                            f_samp=f_samp, 
+                            slope=slope,
+                            **subchunk)
                             self.bin_tod(beam_b, tod=tod_b, add_to_global=True,
                             filter_highpass=filter_highpass,
-                            filter_4fhwp=filter_4fhwp, **subchunk)
+                            filter_4fhwp=filter_4fhwp, 
+                            noise_tod=noise_tod,
+                            sigma=sigma, 
+                            f_min=f_min, 
+                            f_knee=f_knee, 
+                            f_samp=f_samp, 
+                            slope=slope,
+                            **subchunk)
 
     def _chunk2idx(self, **kwargs):
         '''
@@ -4349,6 +4377,7 @@ class ScanStrategy(Instrument, qp.QMap):
 
     def bin_tod(self, beam, tod=None, flag=None, init=True, add_to_global=True,
                 filter_4fhwp=False, filter_highpass=False, noise_tod=False,
+                sigma=None, f_min=None, f_knee=None, f_samp=None, slope=None,
                 **kwargs):
         '''
         Take internally stored tod and boresight
@@ -4411,14 +4440,9 @@ class ScanStrategy(Instrument, qp.QMap):
                 print('adding noise to TOD chunk')
                 nsamp_chunk = len(tod)
                 inp = np.random.normal(0.,1.,(nsamp_chunk,))
-                sigma = 54.0 * 100
-                f_min = 1e-05
-                f_knee = 0.02
-                f_samp = 19.0
-                slope = -2.0
                 gen = ducc0.misc.OofaNoise(sigma, f_min, f_knee, f_samp, slope)
-                noise = gen.filterGaussian(inp)
-                tod += noise
+                noise_chunk = gen.filterGaussian(inp)
+                tod += noise_chunk
                 self.tod = tod 
 
 
